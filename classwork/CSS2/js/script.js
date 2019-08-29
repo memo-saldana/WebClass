b851e30dbeab879028a0e0f9157a06d6980b6c62
Jose Guillermo Saldana <memo.sal98@gmail.com>
2019-08-29 17:23:44 -0500

    Adds value validation

--- classwork/CSS2/js/script.js
+++ classwork/CSS2/js/script.js
@@ -8,4 +8,66 @@ submitButton.addEventListener('click', () => {
   } else {
     name.classList.add('hidden')
   }
+  // validar correo
+  let email = document.getElementById('email');
+  let emailError = document.getElementById('emailError');
+
+  if(email.value == "") {
+    emailError.classList.remove('hidden');
+  } else {
+    if( validaEmail(email.value)) {
+      emailError.classList.add('hidden');
+    } else {
+      emailError.textContent = 'Tu email no es valido'
+    }
+  }
+
+  // validar passwords
+  let password = document.getElementById('password');
+  let passwordError = document.getElementById('passwordError');
+  let passwordConfirmation = document.getElementById('passwordConfirmation');;
+  let passwordConfirmationError = 
+    document.getElementById('passwordConfirmationError');
+
+  if(password.value == "") {
+    passwordError.classList.remove('hidden');
+  } else {
+    passwordError.classList.add('hidden');
+  }
+
+  if(password.value != passwordConfirmation.value ){ 
+    passwordConfirmationError.classList.remove('hidden');
+  } else {
+    passwordConfirmationError.classList.add('hidden');
+  }
+
+  let country = document.getElementById('country');
+  let countryError = document.getElementById('countryError');
+
+  if(country.value == 0) {
+    countryError.classList.remove('hidden');
+  } else {
+    countryError.classList.add('hidden');
+  }
+
+  let radios = document.getElementsByName('gender');
+  let genderError = document.getElementById('genderError')
+  let flag = false;
+
+  for(let i=0; i< radios.length; ++i ) {
+    if( radios[i].checked ) {
+      flag = true;
+    }
+  }
+
+  if(!flag) {
+    genderError.classList.remove('hidden');
+  } else {
+    genderError.classList.add('hidden');
+  }
 });
+
+function validaEmail(email) {
+  var regexpEmail = /\S+@\S+\.\S+/;
+  return regexpEmail.test(email);
+}

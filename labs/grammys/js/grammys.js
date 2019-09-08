303fa61b31df0cb7d214193ead42a5bfb0a27e9d
Jose Guillermo Saldana <memo.sal98@gmail.com>
2019-09-08 18:45:54 -0500

    Changes styling and format of nominee info

--- labs/grammys/js/grammys.js
+++ labs/grammys/js/grammys.js
@@ -14,16 +14,17 @@ dropdown.change( _ => {
   let currentField = fields[fieldId-1];
 
   nominees.html(`<h2> ${currentField.field} </h2>`);
-  nominees.append(`<span> ${currentField.description? currentField.description: ''} </span>`);
+  nominees.append(`<p class="description"> ${currentField.description? currentField.description: ''} </p>`);
   
   currentField.categories.forEach(category => {
     nominees.append(`<h3> ${category.category_name} </h3>`);
     nominees.append(`<p class="description"> ${category.description} </p>`);
-    nominees.append(`<h4> Nominees: </4>`);
     nominees.append(`<ul>`);
-
+//  - ${nominee.artist}: ${nominee.info} 
     category.nominees.forEach((nominee,i) => {
-      nominees.append(`<li class="${i == category.winner_id ? 'winner': ''}"> ${nominee.nominee} - ${nominee.artist}: ${nominee.info} </li>`)
+      nominees.append(`<li> <h4 class="${i == category.winner_id ? 'winner': ''}">${nominee.nominee}  </h4> ${i == category.winner_id? '<span>WINNER!</span>': ''}</li>`);
+      nominees.append(`<p> ${nominee.artist}`);
+      nominees.append(`<p> ${nominee.info}`);
     });
     nominees.append(`</ul>`);
   })
